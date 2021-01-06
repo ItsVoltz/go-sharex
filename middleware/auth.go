baab93d76402fd573294e08229938011aaca9eae
ItsVoltz <29076540+ItsVoltz@users.noreply.github.com>
2021-01-06 01:13:23 -0500

    Update auth.go

--- middleware/auth.go
+++ middleware/auth.go
@@ -9,7 +9,7 @@ import (
 
 func Auth() gin.HandlerFunc {
 	return func (ctx *gin.Context) {
-		if key := ctx.Query("key"); key != "" && config.Get().Keys[key] != "" {
+		if key := ctx.Query("key"); len(key) > 0 && config.Get().Keys[key] != "" {
 			ctx.Next() // key is valid go to next handler
 		} else {
 			ctx.JSON(http.StatusUnauthorized, gin.H{"error": er.AuthInvalidKey}) // no key provided / key is invalid
